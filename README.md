d908e927df82e1b93e67ac518167192d2d0158cd
Ryan P. Nicholl <exaeta@protonmail.com>
2019-04-04 17:18:35 -0400

    First commit, version 1.0.0

---
+++ README.md
@@ -0,0 +1,46 @@
+==Binary Resources for C++ Compiler (BRCC)==
+
+Version 1.0.0.
+
+The Binary Resources for C++ Compiler is a simple cross platform compiler that
+converts binary files to C++ header and source files for inclusion in other 
+projects.
+
+I found other solutions to this problem deficient. xxd doesn't allow you to
+set the name of the variables to anything other than the files name. objdump,
+ld, both have the same issues in general.
+
+BRCC is a cross platform, more predictable solution. It allows you to compile 
+as many binaries into a single output .cpp file as you want, and creates a
+corresponding .hpp file. You can manually set the header guard and it doesn't 
+gum up your code with autogeneration comments.
+
+Usage: brcc output_basename HEADER_GUARD [symbol_prefix binary_file]...
+
+Example: 
+
+    `brcc ./build/gen/binary_resources GAME_BINARY_RESOURCES_HH bin_vertex_shader ./src/vertex_shader.glsl \
+    bin_fragment_shader ./src/fragment_shader.glsl`
+
+Creates files './build/gen/binary_resources.hpp' and './build/gen/binary_resources.cpp'.
+
+The output data:
+
+```lang=cpp
+#ifndef GUARD_HH
+#define GUARD_HH
+#include <cstdint>
+
+extern std::uint8_t const arbitrary_symbol[30];
+extern std::uint8_t const bin_test[13];
+
+#endif
+std::uint8_t const arbitrary_symbol[30] = {
+  0x4c, 0x69, 0x6e, 0x75, 0x78, 0x20, 0x4d, 0x69, 0x6e, 0x74, 0x20, 0x31, 0x38, 0x2e, 0x33, 0x20,
+  0x53, 0x79, 0x6c, 0x76, 0x69, 0x61, 0x20, 0x5c, 0x6e, 0x20, 0x5c, 0x6c, 0x0a, 0x0a
+};
+std::uint8_t const bin_test[13] = {
+  0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x0a
+};
+
+```
---
+++ src/brcc.cpp
@@ -0,0 +1,202 @@
+#include <iostream>
+#include <string>
+#include <exception>
+#include <map>
+#include <cctype>
+#include <utility>
+#include <fstream>
+#include <iterator>
+#include <vector>
+#include <sstream>
+static std::string progname;
+
+static void help()
+{
+  std::cerr << "Binary Resources for C++ Compiler (BRCC) version 1.0.0" << std::endl << std::endl;
+  std::cerr << "Usage: " << progname << " output_basename HEADER_GUARD [symbol_prefix binary_file]..." << std::endl << std::endl;
+  std::cerr << "Example: " <<std::endl << "    " <<  progname << " ./build/gen/binary_resources GAME_BINARY_RESOURCES_HH bin_vertex_shader ./src/vertex_shader.glsl \\" << std::endl <<
+   "    bin_fragment_shader ./src/fragment_shader.glsl" <<std::endl;
+  std::cerr << "  Creates files './build/gen/binary_resources.hpp' and './build/gen/binary_resources.cpp'" << std::endl;
+}
+
+static std::string nicify(std::string const & input)
+{
+  std::string output;
+  
+  for (auto const & x: input)
+  {
+    if (isalnum(x) || x == '_')
+    {
+      output += x;
+      
+    }
+    else output += "_";
+  }
+  
+  return output;
+  
+  
+}
+
+static std::string escape(std::string const & input)
+{
+
+  try
+  
+  {
+    std::string output;
+    
+    if (!isalpha(input.at(0)) && input.at(0) != '_')
+    {
+      throw std::invalid_argument ("expected alpha or underscore");
+    }
+    output += input.at(0);
+    for (size_t i = 1; i < input.size(); i ++)
+    {
+      char c = input.at(i);
+      
+      if (!isalnum(c) && c != '_')
+      {
+        throw std::invalid_argument ("expeced alunum or underscore");
+      }
+      
+      output += c;
+    }
+    
+    
+    return output;
+    
+    
+  }
+  
+  catch (std::out_of_range &)
+  {
+    throw std::invalid_argument("Unexpected end of string");
+  }
+}
+
+
+
+int main(int argc, char **argv)
+{
+  std::map<std::string, std::vector<uint8_t>> str;
+  if (argc >= 1) progname = argv[0];
+  else progname = "<program>";
+  if (argc <= 3) 
+  {
+    help();
+    return 0;
+  }
+  
+  std::string outputname = argv[1];
+  std::string guardname = escape(argv[2]);
+  
+  for (int i = 3; i < argc; i ++)
+  {    
+    std::string symname = argv[i];
+    
+    i++;
+    if (i >= argc) throw std::invalid_argument("expected symbolic pair");
+    std::string fname = argv[i];
+    
+    try
+    {
+      symname = escape(symname);
+    }
+    catch (std::invalid_argument & er)
+    {
+      std::cerr << er.what() << std::endl;
+      return -1;
+    }
+    
+    std::ifstream file(fname, std::ios::binary);
+    if (!file) 
+    {
+      std::cerr << "Error opening file for reading" << std::endl;
+      return -3;
+    }
+    std::vector<uint8_t> content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    
+    str[symname] = std::move(content);
+    
+
+    
+  }
+  
+  std::string header;
+  std::string body;
+  
+  
+  {
+    std::stringstream hout;
+  
+    hout << std::string("#ifndef ")  << nicify(guardname) << std::endl;
+    hout << std::string("#define ") << nicify(guardname) << std::endl;
+    hout << "#include <cstdint>" << std::endl << std::endl;
+    
+    for (auto const & x : str)
+    { 
+      hout << "extern std::uint8_t const " << x.first << "[" << x.second.size() << "];" << std::endl;
+    }
+    
+    hout << std::endl;
+    
+    hout << "#endif" << std::endl;
+    
+    header = hout.str();
+  }
+    
+  {
+    std::stringstream bout;
+    bout << header;
+    
+    for (auto const & x : str)
+    {
+      
+      
+      bout << "std::uint8_t const " << x.first << "[" << x.second.size() << "] = {" << std::endl << "  ";
+      for (size_t y = 0; y < x.second.size(); y++)
+      {
+        bout << "0x";
+        
+        int ca = (x.second[y] & 0xF0) >> 4;
+        int cb = (x.second[y] & 0x0F);
+        
+        auto c = [](char q) -> char
+        {
+          if (q <= 9) return '0' + q;
+          if (q <= 0xF) return 'a' + (q-10);
+          else abort();
+        };
+        
+        bout << c(ca) << c(cb);
+         
+        if (y != x.second.size() - 1)
+        {
+          bout << ",";
+          if (y % 16 == 15)
+          {
+            bout << std::endl << "  ";
+          }
+          else bout << " ";
+        }
+      }
+      bout << std::endl << "};" << std::endl;
+
+    }
+    body = bout.str();
+  }
+  
+  std::ofstream headerf (outputname + ".hpp", std::ios::out | std::ios::trunc);
+  std::ofstream bodyf(outputname + ".cpp", std::ios::out | std::ios::trunc);
+  if (!headerf || !bodyf)
+  {
+    std::cerr << "Error opening file for writing" << std::endl;
+    return -2;
+  }
+  headerf << header;
+  bodyf << body;
+  
+  return 0;
+  
+}
