bd7d8a08db86c4baf1ce4ea69f115d89bde74169
Ryan P. Nicholl <exaeta@protonmail.com>
2019-12-11 17:23:19 -0500

    Version 2.1.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Binary Resources for C++ Compiler (BRCC)
+# Binary Resources for C++ Compiler (BRCXXC)
 
-Version 1.3.2.
+Version 2.1.1
 
 The Binary Resources for C++ Compiler is a simple cross platform compiler that
 converts binary files to C++ header and source files for inclusion in other 
@@ -10,51 +10,11 @@ I found other solutions to this problem deficient. xxd doesn't allow you to
 set the name of the variables to anything other than the files name. objdump,
 ld, both have the same issues in general.
 
-BRCC is a cross platform, more predictable solution. It allows you to compile 
-as many binaries into a single output .cpp file as you want, and creates a
-corresponding .hpp file. You can manually set the header guard and it doesn't 
-gum up your code with autogeneration comments.
 
-Usage: brcc output_basename HEADER_GUARD [-h|-o] [(-c|-i|-s|-v) symbol_prefix binary_file]...
-
-Example: 
-
-    brcc ./build/gen/binary_resources GAME_BINARY_RESOURCES_HH -s bin_vertex_shader ./src/vertex_shader.glsl \
-    bin_fragment_shader ./src/fragment_shader.glsl
-
-Creates files './build/gen/binary_resources.hpp' and './build/gen/binary_resources.cpp', which have std::strings that import the data.
-
-Execution options:
-
-  * `-h` Generate only a header file (does not append .hpp).
-  * `-o` Generate only a source file (does not append .cpp).
-
-Symbol Options:
-
-  * `-s` Stores the binary data in a `std::string`.
-  * `-c` Stores the binary data in a C style array.
-  * `-i` Stores the binary data in a `std::initializer_list<std::uint8_t>`.
-  * `-v` Stores the binary data in a `std::vector<std::uint8_t>`.
-
-Note that the order of options is mandatory, you cannot put `-h` or `-o` in any location except after the header guard.
-
-The output `test-brcc.cpp` file after I ran `brcc ./test-brcc GUARD_HH bin_test ~/test_binary1.bin arbitrary_symbol /etc/issue`:
+Version 2.x.x has been renamed BRCXXC to better reflect that it is intended for 
+use with C++. In addition, there has been a major change to how the compiler is
+run and executed.
 
 ```
-#ifndef GUARD_HH
-#define GUARD_HH
-#include <cstdint>
-
-extern std::uint8_t const arbitrary_symbol[30];
-extern std::uint8_t const bin_test[13];
-
-#endif
-std::uint8_t const arbitrary_symbol[30] = {
-  0x4c, 0x69, 0x6e, 0x75, 0x78, 0x20, 0x4d, 0x69, 0x6e, 0x74, 0x20, 0x31, 0x38, 0x2e, 0x33, 0x20,
-  0x53, 0x79, 0x6c, 0x76, 0x69, 0x61, 0x20, 0x5c, 0x6e, 0x20, 0x5c, 0x6c, 0x0a, 0x0a
-};
-std::uint8_t const bin_test[13] = {
-  0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21, 0x0a
-};
-
+brcxxc HEADER ./build/include/brcxxc.hpp GUARD_BRCXXC_HPP STRING src/glsl/shader1.glsl glsl_shaders::shader1
 ```
---
+++ src/brcxxc.cpp
@@ -0,0 +1,493 @@
+/*
+Binary Resources for C++ Compiler (BRCXXC) version 2.1.1
+Copyright 2019 Ryan P. Nicholl <exaeta@protonmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <locale>
+#include <memory>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <utility>
+
+
+struct fi
+{
+  std::vector<std::string> m_sym;
+  std::string m_head_decl;
+  std::string m_full_decl;
+  std::string m_file_path;
+  bool m_export_dll = false;
+  bool m_import_dll = false;
+};
+
+int main(int argc, char ** argv)
+{
+  int i = 1;
+  
+  enum class mode { header, object };
+  
+  bool implementation = false;
+  
+  
+  mode md;
+  bool export_syms = false;
+  bool import_syms = false;
+st:
+  if (i >= argc)
+  {
+    std::cerr << "Unexpected end of argument list" << std::endl;
+    return -1;
+  }
+  std::string execution_type = std::string(argv[i++]);
+  if (execution_type == "DLLEXPORT_ALL" && export_syms == false)
+  {
+    export_syms = true;
+    goto st;
+  }
+  if (execution_type == "DLLIMPORT_ALL" && export_syms == false)
+  {
+    import_syms = true;
+    goto st;
+  }
+  
+  if (execution_type == "HEADER")
+  {
+    md = mode::header;
+  }
+  else if (execution_type == "OBJECT")
+  {
+    md = mode::object;
+  }
+  else if (execution_type == "IMPLEMENTATION_HEADER")
+  {
+    md = mode::header;
+    implementation = true;
+  }
+  else
+  {
+    std::cerr << "brcxxc: Expected OBJECT or HEADER" << std::endl;
+    return -2;
+  }
+  
+  std::cout << "brcxxc: Execution mode " << execution_type << std::endl;
+  
+   std::cout << "brcxxc: Check Guard" << std::endl;
+
+  std::string guard;
+  
+  
+  if (md == mode::header)
+  {
+
+
+    if (i >= argc) 
+    {
+      std::cerr << "brcxxc: Expected header guard" << std::endl;
+      return -2;
+    }
+    guard = argv[i++];
+    
+
+
+    for (char & c : guard)
+    {
+      if ( !((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c == '_') || ( c >= '0' && c <= '9') ))
+      {
+        c = '_';
+      }
+    }
+
+    std::cerr << "Header guard is: " << guard << std::endl;
+    
+    if (guard.empty())
+    {
+      std::cerr << "brcxxc: Expected header guard" << std::endl;
+      
+      return -3;
+    }
+    
+    if (guard[0] >= '0' && guard[0] <= '9')
+    {
+      std::cerr << "brcxxc: Illegal header guard, identifier cannot start with numbers" << std::endl;
+      
+      return -4;
+    }
+    
+    if (!implementation)
+    {
+      if (guard[0] == '_')
+      {
+        std::cerr << "brcxxc: Illegal header guard, not running in IMPLMENTATION_HEADER mode, see ISO C++ 17.4.3.1.2" << std::endl;
+        
+        return -4;
+      }
+      
+      for (int i = 1; i < guard.size(); i++)
+      {
+        if (guard[i] == '_' && guard[i-1] == '_')
+        {
+          std::cerr << "brcxxc: Illegal header guard, not running in IMPLMENTATION_HEADER mode, see ISO C++ 17.4.3.1.2" << std::endl;
+          return -4;
+        }
+      }
+    }
+    
+  }
+  
+  
+  std::cout << "brcxxc: Header Guard=" << guard << std::endl;
+    
+  
+  if (i >= argc) 
+  {
+    std::cerr << "brcxxc: Expected output file" << std::endl;
+    return -2;
+  }
+  std::string output_file = argv[i++];
+  
+  std::cout << "brcxxc: Output file =" << output_file << std::endl;
+  
+  
+  bool needs_update = false;
+  try
+  {
+    if (! std::filesystem::exists(output_file))
+    {
+      needs_update = true;
+    }
+  }
+  catch (std::exception const & ex)
+  {
+    std::cerr << ex.what() << std::endl;
+    return -1;
+  }
+ 
+    
+  
+  std::set<std::string> includes;
+  
+  std::vector<fi> files;
+  
+  for (i; i+2 < argc; i += 3)
+  {
+    bool dll_export = false;
+    bool dll_import = false;
+  st2:
+    if (i+2 >= argc) break;
+    std::string type    = argv[i+0];
+    std::string filenam = argv[i+1];
+    std::string symbol  = argv[i+2];
+    
+    std::string full_symbol = symbol;
+    std::string short_symbol;
+    
+
+    if (export_syms == true) dll_export = true;
+    if (import_syms == true) dll_import = true;
+
+    if (type == "DLLEXPORT" && dll_export == false)
+    {
+      dll_export = true;
+      i++;
+      goto st2;
+    }
+    if (type == "DLLIMPORT" && dll_import == false)
+    {
+      dll_import = true;
+      i++;
+      goto st2;
+    }
+
+    // would be more efficient do this inbetween type and filenam but this looks better
+    
+    std::vector<std::string> symparts;
+    
+    for (size_t at; (at = symbol.find("::")) != std::string::npos; at)
+    {
+      symparts.push_back(symbol.substr(0, at));
+      symbol = symbol.substr(at+2);
+    }
+    
+    short_symbol = std::move(symbol);
+    
+    
+    fi f;   
+    f.m_file_path = filenam;
+    
+    if (dll_export) f.m_export_dll = true;
+    if (dll_import) f.m_import_dll = true;
+
+
+    if (type == "STRING")
+    {
+      includes.insert("<string>");
+      
+      f.m_head_decl = "std::string const " + short_symbol + ";";
+      f.m_full_decl = "std::string const " + full_symbol;
+    }
+    else if (type == "BYTE_VECTOR")
+    {
+      f.m_head_decl = "std::vector<std::byte> const "+ short_symbol + ";";
+      f.m_full_decl = "std::vector<std::byte> const " + full_symbol;
+      
+      includes.insert("<vector>");
+      includes.insert("<cstddef>");
+    }
+    else if (type == "CHAR_VECTOR")
+    {
+      f.m_head_decl = "std::vector<char> const "+ short_symbol + ";";
+      f.m_full_decl = "std::vector<char> const " + full_symbol;
+      includes.insert("<vector>");
+    }
+    else if (type == "U8_VECTOR")
+    {
+      f.m_head_decl = "std::vector<std::uint8_t> const "+ short_symbol + ";";
+      f.m_full_decl = "std::vector<std::uint8_t> const "+ full_symbol + ";";
+      includes.insert("<vector>");
+      includes.insert("<cinttypes>");
+    }
+    else if (type == "C_U8_ARRAY")
+    {
+      std::size_t sz;
+      
+      sz = std::filesystem::file_size(filenam);
+      std::stringstream ss;
+      ss.imbue(std::locale::classic());
+      ss << "char " << short_symbol << "[" << sz << "]";
+      
+      f.m_head_decl = ss.str() + ";";
+      
+      if (md == mode::object) f.m_full_decl = ss.str();
+      //includes.insert("<inttypes.h>");
+    }
+    
+    else if (type == "BYTE_ARRAY")
+    {
+      std::size_t sz;
+      
+      sz = std::filesystem::file_size(filenam);
+      std::stringstream ss;
+      ss.imbue(std::locale::classic());
+      ss << "std::array<std::byte, "<<sz<<"> " << short_symbol << ";";
+      f.m_head_decl = ss.str();
+      ss.str("");
+      ss << "std::array<std::byte, "<<sz<<"> " << full_symbol;
+      f.m_full_decl = ss.str();
+      if (md == mode::object) f.m_full_decl = ss.str();
+      
+      includes.insert("<array>");
+      includes.insert("<cstddef>");
+    }
+    
+    else if (type == "U8_ARRAY")
+    {
+      std::size_t sz;
+      
+      sz = std::filesystem::file_size(filenam);
+      std::stringstream ss;
+      ss.imbue(std::locale::classic());
+      ss << "std::array<std::uint8_t, " << sz << "> " << short_symbol << ";";            
+      f.m_head_decl = ss.str();
+      ss.str("");
+      ss << "std::array<std::uint8_t, " << sz << "> " << full_symbol << ";";            
+      
+      
+      
+      if (md == mode::object) f.m_full_decl = ss.str();
+      includes.insert("<vector>");
+      includes.insert("<cinttypes>");
+    }
+    
+    f.m_sym = std::move(symparts);
+    
+    
+    
+    std::cerr << "detected symbol head=" << f.m_head_decl << "  full=" << f.m_full_decl <<std::endl;
+    
+    files.push_back(std::move(f));
+    
+    
+    
+  }
+  
+  
+  std::vector<std::string> current_namespace;
+  std::stringstream output;
+  if (md == mode::header)
+  {
+    output << "#ifndef " << guard << std::endl;
+    output << "#define " << guard << std::endl;
+  }
+  
+  for (auto const & x : includes)
+  {
+    output << "#include " << x << std::endl;
+  }
+  
+  
+  for (auto const & x : files)
+  {
+    while (x.m_sym.size() < current_namespace.size())
+    {
+      for (int i = 1; i < current_namespace.size(); i++)
+      {
+        output << "  ";
+      }
+      output << "}" << std::endl;
+      
+      current_namespace.pop_back();
+    }
+    auto it_end = std::mismatch(current_namespace.begin(), current_namespace.end(), x.m_sym.begin());
+    
+    std::size_t dist1 = std::distance(current_namespace.begin(), it_end.first);
+    
+    while (dist1 < current_namespace.size())
+    {
+      std::cout << "B" << std::endl;
+      for (int i = 1; i < current_namespace.size(); i++)
+      {
+        output << "  ";
+      }
+      output << "}" << std::endl;
+      
+      current_namespace.pop_back();
+    }
+    
+    while (it_end.second != x.m_sym.end())
+    {
+      for (int i = 0; i < current_namespace.size(); i++)
+      {
+        output << "  ";
+      }
+      output << "namespace " << *it_end.second << std::endl;
+      for (int i = 0; i < current_namespace.size(); i++)
+      {
+        output << "  ";
+      }
+      output << "{" << std::endl;
+      current_namespace.push_back(*it_end.second);
+      
+      it_end.second++;
+      
+    }
+    
+    for (int i = 0; i < current_namespace.size(); i++)
+    {
+        output << "  ";
+    }
+    output << "extern ";
+
+    if (x.m_export_dll) output << "__declspec(dllexport) ";
+    if (x.m_import_dll) output << "__declspec(dllimport) ";
+
+
+    output << x.m_head_decl << std::endl;
+
+  }
+
+
+  while (current_namespace.size() != 0)
+  {
+    for (int i = 1; i < current_namespace.size(); i++)
+    {
+      output << "  ";
+    }
+    output << "}" << std::endl;
+    
+    current_namespace.pop_back();
+  }
+
+  
+  if (md == mode::header)
+  {
+    output << "#endif" << std::endl;
+  }
+  
+  if (md == mode::object) for (auto const & x : files)
+  {
+    output << x.m_full_decl;
+
+    std::cerr << "x.m_fulldecl=" << x.m_full_decl <<std::endl;
+
+    std::ifstream file( x.m_file_path, std::ios::binary );
+
+    typename std::vector<char> vec( (std::istreambuf_iterator<char>(file)), (std::istreambuf_iterator<char>()));
+
+    if (vec.size() < 20)
+    {
+      output << " { ";
+
+      for (size_t i = 0; i < vec.size(); ++i)
+      {
+        output << "0x";
+
+        int a = 0;
+        int b = 0;
+        a = uint8_t(vec[i]) & 0x0F;
+        b = uint8_t(vec[i]) & 0xF0;
+        b = b >> 4;
+
+        char ca = 0;
+        char cb = 0;
+        if (a < 10) ca = '0' + a;
+        else ca = 'A' + a - 10;
+
+        if (b < 10) cb = '0' + b;
+        else cb = 'A' + b - 10;
+
+        output << ca << cb;
+
+        if (i != vec.size() - 1) output << ", ";
+        else output << " };" << std::endl;
+      }
+
+    }
+    else
+    {
+      output << " {" << std::endl;
+      for (size_t i = 0; i < vec.size(); i += 20)
+      {
+        output << "  ";
+        for (size_t z = i; z < vec.size() && z < i + 20; z++)
+        {
+          output << "0x";
+
+          int a = 0;
+          int b = 0;
+          a = uint8_t(vec[z]) & 0x0F;
+          b = uint8_t(vec[z]) & 0xF0;
+          b = b >> 4;
+
+          char ca = 0;
+          char cb = 0;
+          if (a < 10) ca = '0' + a;
+          else ca = 'A' + a - 10;
+
+          if (b < 10) cb = '0' + b;
+          else cb = 'A' + b - 10;
+
+          output << ca << cb;
+
+          if (z == vec.size() -  1) output << std::endl << "};" << std::endl;
+          else if (z == i+20-1) output << "," << std::endl;
+          else output << ", ";
+        }
+      }
+    }
+
+  }
+
+  std::cout << output.str() << std::endl;
+  
+  
+}
